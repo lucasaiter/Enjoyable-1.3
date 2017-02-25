a950fd4d1446b6e7576891bc51acb8a6b7e08eb0
Sam Deane <sam@elegantchaos.com>
2017-02-25 18:46:52 +0000

    Better handling for buttons with analogue values. Treat any input > min as active, rather than only the max value.

--- Classes/NJInputButton.m
+++ Classes/NJInputButton.m
@@ -7,7 +7,11 @@
 
 #import "NJInputButton.h"
 
+// #define TraceValue NSLog
+#define TraceValue(...) do {} while(0)
+
 @implementation NJInputButton {
+    CFIndex _min;
     CFIndex _max;
 }
 
@@ -19,18 +23,22 @@
                                 eid:NJINPUT_EID("Button", index)
                             element:element
                              parent:parent])) {
+        _min = IOHIDElementGetLogicalMin(element);
         _max = IOHIDElementGetLogicalMax(element);
     }
     return self;
 }
 
 - (id)findSubInputForValue:(IOHIDValueRef)val {
-    return (IOHIDValueGetIntegerValue(val) == _max) ? self : nil;
+    TraceValue(@"%ld", IOHIDValueGetIntegerValue(val));
+    return (IOHIDValueGetIntegerValue(val) != _min) ? self : nil;
 }
 
-- (void)notifyEvent:(IOHIDValueRef)value {
-    self.active = IOHIDValueGetIntegerValue(value) == _max;
-    self.magnitude = IOHIDValueGetIntegerValue(value) / (float)_max;
+- (void)notifyEvent:(IOHIDValueRef)valueRef {
+    CFIndex value = IOHIDValueGetIntegerValue(valueRef);
+    self.active = value != _min;
+    self.magnitude = value / (float)_max;
+    TraceValue(@"%ld %d %f", value, self.active, self.magnitude);
 }
 
 @end
